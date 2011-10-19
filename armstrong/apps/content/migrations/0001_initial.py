f283b25ebc198252b8be0b8cc6ba1c7308d9402b
Dave McLain <dmclain@texastribune.org>
2011-10-19 16:54:08 -0500

    add dependencies

--- armstrong/apps/content/migrations/0001_initial.py
+++ armstrong/apps/content/migrations/0001_initial.py
@@ -7,6 +7,11 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+            ("arm_access", "0001_initial"),
+            ("arm_sections", "0001_initial"),
+    )
+
     def forwards(self, orm):
         
         # Adding model 'Content'
